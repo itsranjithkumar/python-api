a4702ef5be6d0b9619d39501599dd80fb73cb51b
itsranjithkumar <ranjithkumarms28@gmail.com>
2024-03-28 18:36:58 +0530

    python 2:00:30

---
+++ main.py
@@ -0,0 +1,53 @@
+
+from argparse import OPTIONAL
+from typing import Optional
+from fastapi import Body, FastAPI,Response,status,HTTPException
+from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
+
+
+app = FastAPI()
+
+class post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional [int] = None 
+
+   
+my_posts = [{"title":"title of post 1", "content":"content of post 1", "id":1}, {"title":"favorite food", "content":"I like pizza", "id":2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
+@app.get("/")
+def root():
+
+    return {"message": "Hello World"}
+
+@app.get("/posts")
+def get_posts():
+    return{"data": my_posts}
+
+@app.post("/posts")
+def create_posts(post:post):
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 1000000) 
+    my_posts.append(post_dict)
+    return {"data":post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id:int):
+    
+    post = find_post(id)
+    if not post:
+        if not post:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+       
+    return {"post_detail": post}
+    
+
