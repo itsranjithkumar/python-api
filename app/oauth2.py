9b3e582a5c6f27b8da5979f904b4239706d9181e
itsranjithkumar <ranjithkumarms28@gmail.com>
2024-04-03 11:57:54 +0530

    bug

--- app/oauth2.py
+++ app/oauth2.py
@@ -18,9 +18,9 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def create_access_token(data: dict):
-    to_encode = data.copy()
+    to_encode = data.copy() 
 
-    expire = datetime.datatime() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.datetime() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
     encoded_jwt=jwt.encode(to_encode, SECRET_KEY,algorithm=ALGORITHM)
@@ -33,18 +33,20 @@ def verify_access_token(token:str, credentials_exception):
       
     payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
-    id: str = payload.get("users_id")
+    id: str = payload.get("user_id")
 
     if id is None:
      raise credentials_exception
     token_data = schemas.TokenData(id=id)
-  except JWTError: 
-    raise credentials_exception
-  
+  except JWTError as e: 
+      print(e)
+      raise credentials_exception
+  except AssertionError as e:
+      print(e)
   return token_data
   
 
 def get_current_user(token:str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(status.HTTP_401_UNAUTHORIZED, detail=f"could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
-
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+    
     return verify_access_token(token, credentials_exception)
