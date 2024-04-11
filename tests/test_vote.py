fcc87f78721476d3c517e190e0f7d7ec076bef9c
itsranjithkumar <ranjithkumarms28@gmail.com>
2024-04-11 16:40:10 +0530

    bug fixed

--- tests/test_vote.py
+++ tests/test_vote.py
@@ -1,11 +1,9 @@
 import pytest
 from app import models
 
-
-
 @pytest.fixture()
 def test_vote(test_posts, session, test_user):
-    new_vote = models.Votes(post_id=test_posts[3].id, user_id=test_user['id'], dir=1)
+    new_vote = models.Votes(post_id=test_posts[3].id,user_id=test_user['id'])
     session.add(new_vote)
     session.commit()
 
